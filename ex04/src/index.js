84cb4423d9e7e4bb9001b77c815c4351b99a37da
amnazu <amna_zukic@hotmail.com>
2021-03-10 22:17:55 +0100

    treći dan

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main () {
+
+    var x;
+    var y; 
+
+    x = 10;
+    y = x;
+
+     
+return [x, y];
+}
+
+console.log(main());
+
+module.exports = main;  
---
+++ ex08/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    
+var numOne;
+var numTwo;
+var sum;
+
+sum = numOne + numTwo;
+return sum;
+
+}
+
+console.log(main(15, 8)); //23;
+console.log(main(5, 28)); //33;
+console.log(main(150, 425)); //575;
+
+module.exports = main;
+
+
+    
